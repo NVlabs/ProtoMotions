c3e94d41abbfbe91ab72d33e34d605a87a9d6bf0
mael <a974612694@outlook.com>
2025-05-04 04:56:39 +0400

    unify the loading codes with torch.load(checkpoint_path, map_location=self.fabric.device)

--- protomotions/agents/masked_mimic/agent.py
+++ protomotions/agents/masked_mimic/agent.py
@@ -88,7 +88,6 @@ class MaskedMimic(PPO):
             checkpoint_path = self.config.expert_model_path + "/score_based.ckpt"
             if not Path(checkpoint_path).exists():
                 checkpoint_path = self.config.expert_model_path + "/last.ckpt"
-            pre_trained_expert = torch.load(checkpoint_path, map_location='cpu')
 
             self.expert_model_config = OmegaConf.load(
                 Path(self.config.expert_model_path) / "config.yaml"
@@ -108,12 +107,12 @@ class MaskedMimic(PPO):
             expert_model: PPOModel = instantiate(
                 self.expert_model_config.agent.config.model
             )
-            
-            # load parameters before setting up the model
-            expert_model.load_state_dict(pre_trained_expert["model"])
-
             self.expert_model = self.fabric.setup(expert_model)
             self.expert_model.mark_forward_method("act")
+
+            # loading should be done after fabric.setup to ensure the model is on the correct fabric.device
+            pre_trained_expert = torch.load(checkpoint_path, map_location=self.fabric.device) 
+            self.expert_model.load_state_dict(pre_trained_expert["model"])
             for param in self.expert_model.parameters():
                 param.requires_grad = False
             self.expert_model.eval()  # Just incase
